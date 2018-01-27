b3667a097d190b5c521ea5ffac3c606953c5bd36
maciej <maciej@maciej.maciej>
2018-01-27 15:00:43 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# t-grid
+T-grid is a responsive framework based on display table (oldschool!). It can be an interesting alternative for flex-boxes, float rows or css-grid. It should be used whenever you need to achieve a compromise between design and older browsers usability.
+
+In source you can find sass version of framework which can be used to your own layout customization.
+
+#### Check ```getting-started``` folder to learn more and to see a tutorial!
---
+++ source/js/t-grid.js
@@ -0,0 +1,35 @@
+function tGrid() {
+
+    var self = this;
+
+    /* Create method that fixes nested grids, immediately launch it and add event listener to resize. */
+    this.fixNestedGrids = function () {
+
+        /* Get all elements that potentially need fixing... */
+        var $toExamine = $('.t-col > .t-grid.t-fill');
+
+        /* ...and examine them */
+        $toExamine.each(function () {
+
+            $this = $(this);
+
+            /* Remove height styles added previously to this element. */
+            $this.css({
+                'height': ''
+            });
+
+            /* Check if its height is less than its parent. */
+            var parentHeight = $this.closest('.t-col').outerHeight();
+
+            if (parseInt($this.css('height')) < parentHeight) {
+                $this.css({
+                    'height': parentHeight
+                });
+            }
+        });
+
+    }
+    this.fixNestedGrids();
+    $(window).resize(this.fixNestedGrids);
+
+}
